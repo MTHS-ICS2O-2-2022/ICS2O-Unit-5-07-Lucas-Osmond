ec93fb8731c4f266db4a1511c54e8c73d106cc04
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-11 17:14:19 +0000

    finished 5-07

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Osmond
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  const number = parseInt(document.getElementById('number').value)
+  let answer = 0
+
+  for (let counter = 0; counter <= number; counter++) {
+    answer += counter
+  }
+  document.getElementById('answer').innerHTML = 'The sum of all numbers from 0 to ' + number + ' is ' + answer + '.'
+}
